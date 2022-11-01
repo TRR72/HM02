94eacb7cff866d0c63386ef2a5b9fe4a8f5ea0f5
TRR72 <116944430+TRR72@users.noreply.github.com>
2022-11-01 13:54:24 +0500

    HM07

---
+++ HM07/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 1 && day <= 7)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Выходной день");
+    } 
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
+else
+{
+    Console.WriteLine("Вы вышли из диапазона от 1 до 7");
+}
